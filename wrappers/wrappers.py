365b9773bc47ef9726b2a769dc2ed02352edd5be
mihnea <13464649+umihnea@users.noreply.github.com>
2020-08-11 19:40:54 +0300

    Remove action repeater
    the gym env will take care of that

--- wrappers/wrappers.py
+++ wrappers/wrappers.py
@@ -7,51 +7,6 @@ import gym
 from gym.wrappers import Monitor
 
 
-class RepeatActionAndMaxFrame(gym.Wrapper):
-    def __init__(
-        self, env=None, repeat=4, clip_reward=False, no_ops=0, fire_first=False
-    ):
-        super(RepeatActionAndMaxFrame, self).__init__(env)
-        self.repeat = repeat
-        self.shape = env.observation_space.low.shape
-        self.frame_buffer = np.zeros_like((2, self.shape))
-        self.clip_reward = clip_reward
-        self.no_ops = no_ops
-        self.fire_first = fire_first
-
-    def step(self, action):
-        t_reward = 0.0
-        done = False
-        for i in range(self.repeat):
-            obs, reward, done, info = self.env.step(action)
-            if self.clip_reward:
-                reward = np.clip(np.array([reward]), -1, 1)[0]
-            t_reward += reward
-            idx = i % 2
-            self.frame_buffer[idx] = obs
-            if done:
-                break
-
-        max_frame = np.maximum(self.frame_buffer[0], self.frame_buffer[1])
-        return max_frame, t_reward, done, info
-
-    def reset(self):
-        obs = self.env.reset()
-        no_ops = np.random.randint(self.no_ops) + 1 if self.no_ops > 0 else 0
-        for _ in range(no_ops):
-            _, _, done, _ = self.env.step(0)
-            if done:
-                self.env.reset()
-        if self.fire_first:
-            assert self.env.unwrapped.get_action_meanings()[1] == "FIRE"
-            obs, _, _, _ = self.env.step(1)
-
-        self.frame_buffer = np.zeros_like((2, self.shape))
-        self.frame_buffer[0] = obs
-
-        return obs
-
-
 class PreprocessFrame(gym.ObservationWrapper):
     def __init__(self, shape, env=None):
         super(PreprocessFrame, self).__init__(env)
@@ -96,20 +51,13 @@ class StackFrames(gym.ObservationWrapper):
 
 
 def make_env(
-    env_name,
-    shape=(84, 84, 1),
-    repeat=4,
-    clip_rewards=False,
-    no_ops=0,
-    fire_first=False,
-    monitor_path=None,
+    env_name, shape=(84, 84, 1), repeat=4, monitor_path=None,
 ):
     env = gym.make(env_name)
 
     if monitor_path is not None:
         env = Monitor(env, monitor_path, force=True)
 
-    env = RepeatActionAndMaxFrame(env, repeat, clip_rewards, no_ops, fire_first)
     env = PreprocessFrame(shape, env)
     env = StackFrames(env, repeat)
 
